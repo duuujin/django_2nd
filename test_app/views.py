c853430bc7adb840cc2fed406c11811f37c689fc
DESKTOP-BTDJ0S7\Administrator <enwls5626@gmail.com>
2025-10-25 22:19:13 +0900

    수정,삭제

--- test_app/views.py
+++ test_app/views.py
@@ -29,3 +29,26 @@ def detail(request, test_pk):
         'test' : test
     }
     return render(request, 'test_app/detail.html', context)
+
+def delete(request, test_pk):
+    test = Test.objects.get(pk=test_pk)
+    if request.method == "POST":
+        test.delete()
+        return redirect('test_app:index')
+    else:
+        return redirect('test_app:detail', test_pk)
+
+def update(request, test_pk):
+    test = Test.objects.get(pk=test_pk)
+    if request.method == "POST":
+        form = TestForm(request.POST, instance=test)
+        if form.is_valid():
+            test = form.save()
+            return redirect('test_app:detail', test_pk)
+    else:
+        form = TestForm(instance=test)
+    context = {
+        'tests' : test,
+        'form' : form
+    }
+    return render(request, 'test_app/update.html', context)
